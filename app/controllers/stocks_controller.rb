6379c5231ccc2f259091545c1c3a21cf93f2d9f5
Vincent <vincethewalker@yahoo.co.uk>
2020-09-17 15:17:20 +0100

    Created Controller action to display stock, price names

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,8 +1,9 @@
 class StocksController < ApplicationController
 
     def search
-      stock = Stock.new_lookup(params[:stock])
-      render json: stock
+      @stock = Stock.new_lookup(params[:stock])
+      #render json: @stock (shows view in JSON format)
+      render 'users/my_portfolio'
     end
 
 end
