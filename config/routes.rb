d845568924be1bbb1b60aeea67e7145bfef8ecf8
Vincent <vincethewalker@yahoo.co.uk>
2020-09-17 13:55:35 +0100

    Added controller features for Stock Price

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'welcome/index'
   get 'my_portfolio', to: 'users#my_portfolio'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'search_stock', to: 'stocks#search'
 end
