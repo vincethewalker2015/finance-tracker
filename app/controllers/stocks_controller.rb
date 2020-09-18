2c3dc72ca905af47f0ab07661b65e05d352a5cf1
Vincent <vincethewalker@yahoo.co.uk>
2020-09-18 11:45:41 +0100

    Created JS response to handle AJAX query in portfolio page

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -5,7 +5,10 @@ class StocksController < ApplicationController
         @stock = Stock.new_lookup(params[:stock])
         if @stock # This will check to see (As in model if a stock value exists)
           #render json: @stock (shows view in JSON format)
-          render 'users/my_portfolio'
+          #render 'users/my_portfolio' (This renders in HTML using _result.html.erb but we need to format this in JS )
+          respond_to do |format|
+            format.js { render partial: 'users/result' } # (This renders in JS using _result.js.erb)
+          end
         else
           flash[:alert] = "Please enter a valid symbol to search"
           redirect_to my_portfolio_path
