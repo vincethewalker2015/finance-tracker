0c81260eec37b3feb58636525e96350173465fac
Vincent <vincethewalker@yahoo.co.uk>
2020-09-18 12:15:43 +0100

    Created JS Response to handle incorrect entry and non entry

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -5,17 +5,25 @@ class StocksController < ApplicationController
         @stock = Stock.new_lookup(params[:stock])
         if @stock # This will check to see (As in model if a stock value exists)
           #render json: @stock (shows view in JSON format)
-          #render 'users/my_portfolio' (This renders in HTML using _result.html.erb but we need to format this in JS )
+          #render 'users/my_portfolio' (This renders in HTML using _result.html.erb and reloads the page but we need to format this in JS )
           respond_to do |format|
             format.js { render partial: 'users/result' } # (This renders in JS using _result.js.erb)
           end
         else
-          flash[:alert] = "Please enter a valid symbol to search"
-          redirect_to my_portfolio_path
+          #flash[:alert] = "Please enter a valid symbol to search" (This renders the response in HTML and reloads the page)
+          respond_to do |format|
+            flash.now[:alert] = "Please enter a valid symbol to search"
+            format.js { render partial: 'users/result' } # (This renders in JS using _result.js.erb)
+          end
+          #redirect_to my_portfolio_path
         end
       else
-        flash[:alert] = "Please enter a Symbol to search"
-        redirect_to my_portfolio_path
+        #flash[:alert] = "Please enter a Symbol to search" (This Renders in HTML and has the page reloaded)
+        #redirect_to my_portfolio_path
+        respond_to do |format|
+          flash.now[:alert] = "Please enter a Symbol to search"
+          format.js { render partial: 'users/result' } # (This renders in JS using _result.js.erb)
+        end
       end
 
     end
