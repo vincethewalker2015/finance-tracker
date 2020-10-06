5f3bd6d55a3b27900f782f2cb81ef0821ba59b85
Vincent <vincethewalker@yahoo.co.uk>
2020-10-06 16:11:54 +0100

    Added friendships table and Model

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,9 @@ Rails.application.routes.draw do
   get 'welcome/index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
-
+  get 'my_friends', to: 'users#my_friends'
+  get 'search_friend', to: 'users#search'
   get 'chomp', to: 'stocks#chomp'
+
+  
 end
