ad62801445330d9e955dcfd710aeb8fcc45bd1a9
Vincent <vincethewalker@yahoo.co.uk>
2020-10-07 17:13:24 +0100

    Added Friend Search form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,28 @@ class UsersController < ApplicationController
     @friends = current_user.friends
   end
 
-  def search 
-    render json: params[:friend]
-  end
+  # def search 
+  #   render json: params[:friend]
+  # end
+
+  def search
+    if params[:friend].present?
+      @friend = params[:friend]
+        if @friend
+          respond_to do |format|
+            format.js { render partial: 'users/friend_result' }
+          end
+        else
+          respond_to do |format|
+            flash.now[:alert] = "User is not found.."
+            format.js { render partial: 'users/friend_result' }
+          end
+        end
+      else
+        respond_to do |format|
+          flash.now[:alert] = "Please enter a friend name or email to search"
+          format.js { render partial: 'users/friend_result' }
+        end
+      end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,8 @@ Rails.application.routes.draw do
   get 'search_stock', to: 'stocks#search'
   get 'my_friends', to: 'users#my_friends'
   get 'search_friend', to: 'users#search'
-  get 'chomp', to: 'stocks#chomp'
+  # resources :friendships, only: [:create, :destroy]
+  # resources :users, only: [:show]
 
   
 end
