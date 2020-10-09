52d72181fcb9cdabe144c26a954251716bd6114b
Vincent <vincethewalker@yahoo.co.uk>
2020-10-09 19:01:13 +0100

    CSS Styling Changes to the front end before deploy

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
   resources :user_stocks, only: [:create, :destroy]
   get 'users/my_portfolio'
   devise_for :users
-  root 'welcome#index'
+  root'welcome#index'
+  get 'about', to: 'welcome#about'
   get 'welcome/index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
